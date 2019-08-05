e7db11180cd5e81605218fdb22ff6b37e84f3384
Arvind Thirumurugan <tionarvind@ymail.com>
2019-08-05 13:11:05 -0400

    Update Group_Anagrams.java

--- Group_Anagrams.java
+++ Group_Anagrams.java
@@ -1,3 +1,5 @@
+//Solution 1 using sorting
+
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         int i;
@@ -25,6 +27,52 @@ class Solution {
             }
         }
         for(Map.Entry<String,List<String>> entry: map.entrySet())
+        {
+            result.add(entry.getValue());
+        }
+        return result;
+    }
+}
+
+// Solution 2 using generated hash values for strings
+
+class Solution {
+    public int getKey(String value)
+    {
+        int i,c;
+        int sum=0;
+        for(i=0;i<value.length();i++)
+        {
+            c = value.charAt(i);
+            sum = sum+(int)Math.pow(c,4);
+        }
+        return sum;
+    }
+    public List<List<String>> groupAnagrams(String[] strs) {
+        int i;
+        int key;
+        List<String> anagramList;
+        List<List<String>> result = new ArrayList<>();
+        Map<Integer,List<String>> map = new HashMap<Integer,List<String>>();
+        for(i=0;i<strs.length;i++)
+        {
+            key = getKey(strs[i]);
+            //System.out.println(key);
+            if(map.containsKey(key) == false)
+            {
+                anagramList = new ArrayList<String>();
+                anagramList.add(strs[i]);
+                map.put(key,anagramList);
+            }
+            else
+            {
+                anagramList = map.get(key);
+                anagramList.add(strs[i]);
+                map.put(key,anagramList);
+            }
+            //System.out.println(map);
+        }
+        for(Map.Entry<Integer,List<String>> entry : map.entrySet())
         {
             result.add(entry.getValue());
         }
