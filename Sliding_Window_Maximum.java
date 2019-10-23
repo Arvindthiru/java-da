f0cd9f2ab79490a3e0c04ddf15d5b66f909916ec
Arvind Thirumurugan <athirumu@buffalo.edu>
2019-10-23 00:52:03 -0400

    Update Sliding_Window_Maximum.java

--- Sliding_Window_Maximum.java
+++ Sliding_Window_Maximum.java
@@ -1,85 +1,41 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        int max,i,j;
-        max = 0;
         if(nums.length == 0)
         {
             return new int[0];
         }
-        if(nums.length<=k)
-        {
-            int[] finalResult = new int[1];
-            for(i=0;i<nums.length;i++)
-            {
-                if(nums[i]>=nums[max])
-                {
-                    max = i;
-                    finalResult[0] = nums[max];
-                }
-            }
-            return finalResult;
-        }
+        int i;
+        ArrayDeque<Integer> dq = new ArrayDeque<>();
         ArrayList<Integer> result = new ArrayList<>();
-        ArrayDeque<Integer> deque = new ArrayDeque<>();
-        //For the first window
+        int[] ans;
+        dq.addLast(0);
         for(i=1;i<k;i++)
         {
-            if(nums[i] > nums[max])
+            while(dq.isEmpty() == false && nums[i] >= nums[dq.getLast()])
             {
-                max = i;
+                dq.pollLast();    
             }
+            dq.addLast(i);
         }
-        deque.addFirst(max);
-        result.add(nums[max]);
-        //For the rest of the windows
-        for(i=1;i<=nums.length-k;i++)
+        result.add(nums[dq.getFirst()]);
+        for(i=k;i<nums.length;i++)
         {
-            //System.out.println(deque);
-            max = cleanDeque(deque,nums,i,max);
-            j = i;
-            while(j<i+k)
+            if(dq.getFirst() <= i-k)
             {
-                if(nums[j]>nums[max])
-                {
-                    deque.add(i);
-                    max = j;
-                }
-                j++;
+                dq.pollFirst();
             }
-            result.add(nums[max]);
-        }
-        //System.out.println(result);
-        int[] finalResult = new int[result.size()];
-        i=0;
-        for(Integer entry : result)
-        {
-            finalResult[i] = result.get(i);
-            i++;
-        }
-        return finalResult;
-        
-    }
-    public int cleanDeque(ArrayDeque<Integer> deque,int[] nums,int i, int max)
-    {
-
-        if(deque.isEmpty()==false && deque.getFirst()<i)
-        {
-            deque.removeFirst();
-            max = i;
-            deque.addLast(i);
-        }
-        if(deque.isEmpty() == true)
-        {
-            max = i;
-            deque.addLast(i);
-        }
-        if(deque.isEmpty() == false)
-        {
-            while(deque.isEmpty() == false && nums[deque.getFirst()]<nums[max])
+            while(dq.isEmpty()==false && nums[dq.getLast()] < nums[i])
             {
-                deque.removeFirst();
+                dq.pollLast();
             }
+            dq.addLast(i);
+            result.add(nums[dq.getFirst()]); 
         }
-        return max;
+        ans = new int[result.size()];
+        for(i = 0;i<result.size();i++)
+        {
+            ans[i] = result.get(i);
+        }
+        return ans;
     }
 }
